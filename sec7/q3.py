b9e4cd7a1cbd6b3ef41134086778d2e4204e0b46
Reza <effati78@protonmail.com>
2021-11-27 09:44:37 +0330

    The questions of the seventh part

---
+++ sec7/q3.py
@@ -0,0 +1,17 @@
+# سوال سه
+
+str = input()
+n = int(input("n: "))
+isupper_count = 0
+islower_count = 0
+
+for x in range(n):
+    if(str[x].isupper()):
+        isupper_count += 1        
+
+    if(str[x].islower()):
+        islower_count += 1  
+
+print('isupper:', isupper_count)
+print('islower:', islower_count)
+        
---
+++ sec7/q4.py
@@ -0,0 +1,13 @@
+# سوال چهار
+
+A = {'a', 'd', 'c'}
+B = {'a', 'b', 'h'}
+
+q = A & B #q = (A ∩ B)
+z = A | B #z = (A ∪ B)
+w = A - B #w = (A - B)
+
+# (A ∩ B) ∪ (A - B) => q ∪ w
+o = q | w # q ∪ w
+
+print(o)
