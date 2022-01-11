3e9963547b059a4ddd649733df04f89fc8dd0a41
Reza <effati78@protonmail.com>
2022-01-11 17:58:35 +0330

    project added

---
+++ prj/prj1.py
@@ -0,0 +1,11 @@
+import turtle
+
+t = turtle.Turtle()
+list1 = ["purple", "red", "orange", "blue", "green"]
+for i in range(200):
+    t.color(list1[i % 5])
+    t.pensize(i / 10 + 1)
+    t.forward(i)
+    t.left(59)
+    
+turtle.exitonclick()
